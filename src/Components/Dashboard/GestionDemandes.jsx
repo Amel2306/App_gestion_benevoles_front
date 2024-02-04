bcde4332a22feb66f47cb73e4b27b634bead6b28
Amel <ameladdou213@gmail.com>
2024-02-04 14:16:21 +0100

    erreur nommage fichieré

--- src/Components/Dashboard/GestionDemandes.jsx
+++ src/Components/Dashboard/GestionDemandes.jsx
@@ -1,134 +1,90 @@
 import React, { useEffect, useState } from 'react';
-import { XMarkIcon } from "@heroicons/react/24/solid";
-import { useNavigate } from 'react-router-dom';
 import axiosInstance from '../../config/axiosConfig';
+import '../ProfilPage/ModifyProfilPage.css'
+import VoirDemandes from '../Informations/VoirDemandes';
 
-const VoirDemandes = ({ selectedList, pseudoUser, handleClose }) => {
-    const navigate = useNavigate();
-    const [creneauxInfo, setCreneauxInfo] = useState({});
-    const [zoneInfo, setZoneInfo] = useState({})
+const GestionDemandes = () => {
+    const [usersWithDemands, setUsersWithDemands] = useState({});
+    const [users, setUsers] = useState({});
+    const [voirDemandes,setVoirdemandes] = useState(false);
+    const [selectedList, setSelectedList] = useState([])
+    const [pseudoUser, setPseudoUser] = useState("")
+
 
     useEffect(() => {
-        async function fetchData() {
+        const fetchData = async () => {
             try {
-                const creneauxResponse = await axiosInstance.get('creneaux');
-                const creneaux = creneauxResponse.data;
-                console.log(creneaux);
+                const usersResponse = await axiosInstance.get('users');
+                const allUsers = usersResponse.data;
 
-                const newCreneauxInfo = {};
-                const joursSemaine = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
+                const usersWithDemandsData = {};
+                const usersProv = {};
+                for (const user of allUsers) {
+                    const demandsResponse = await axiosInstance.get(`demanderactivtie/user/${user.id}`);
+                    const demands = demandsResponse.data;
+                    
+                    if (demands.length > 0) {
+                        usersWithDemandsData[user.id] = demands;
+                        usersProv[user.id] = user;
+                    }
+                }
 
-                for (const creneau of creneaux) {
-                    console.log(creneau)
-                    const jour = joursSemaine[new Date(creneau.date).getDay()];
-                    console.log(jour)
-                    const heureDebut = creneau.horaire_debut.split(':')[0];
-                    const heureFin = creneau.horaire_fin.split(':')[0];
-                    console.log(creneau);
-
-                    const creneauInfoText = `${jour} : ${heureDebut}-${heureFin}`;
-                    console.log(jour);
-                    newCreneauxInfo[creneau.id] = creneauInfoText;
-                };
-
-                setCreneauxInfo(newCreneauxInfo);
+                setUsersWithDemands(usersWithDemandsData);
+                setUsers(usersProv)
             } catch (error) {
-                console.error('Erreur lors de la récupération des informations des créneaux :', error);
+                console.error('Erreur lors de la récupération des données :', error);
             }
-        }
-        async function fetchData2() {
-            try {
-                const creneauxResponse = await axiosInstance.get('zonebenevole');
-                const creneaux = creneauxResponse.data;
-                console.log(creneaux);
-
-                const newCreneauxInfo = {};
-
-                for (const creneau of creneaux) {
-
-                    newCreneauxInfo[creneau.id] = creneau.nom_zb;
-                };
-
-                setZoneInfo(newCreneauxInfo);
-            } catch (error) {
-                console.error('Erreur lors de la récupération des informations des zones :', error);
-            }
-        }
-
-        if (Object.keys(creneauxInfo).length === 0) {
-            fetchData();
-        }
-        fetchData2();
+        };
+        fetchData();
     }, []);
 
-    const handleSubmitValide = async (id) => {
-        try {
-            await axiosInstance.put(`demanderactivtie/accepte/${id}/1`);
-            console.log("La demande a été acceptée avec succès.");
-        } catch (error) {
-            console.error(`Erreur lors de la validation de la demande avec l'ID ${id}:`, error);
-        }
-    };
+    const handleClickDemandes = (liste, pseudoUser) => {
+        setSelectedList (liste)
+        setPseudoUser(pseudoUser)
+        setVoirdemandes(true)
+    }
 
-    const handleSubmitDevalide = (e) => {
-        e.preventDefault();
-    };
+    const handleClose = () => {
+        setSelectedList ([])
+        setVoirdemandes(false)
+    }
 
     return (
-        <div className={`container rounded-lg bg-opacity-85 p-8 shadow-lg m-4 mr-20 ml-20`}>
-            <div className='flex flex-col'>
-                <div className='flex flex-col'>
-                    <div className=' felex flex-row align-right justify-right pl-[500px]'>
-                        <button type="button" className="text-white bg-fuchsia-700 focus:outline-none hover:bg-fuchsia-500 focus:ring-4 focus:ring-white font-medium rounded-full text-sm px-2 py-2 me-2 mb-2 " onClick={handleClose}>
-                            <XMarkIcon className="h-5 w-5 inline-block" />
-                        </button>
-                    </div>
-                    <h2 className="text-2xl font-bold text-indigo-900 mb-5">Demande de {pseudoUser}</h2>
+        <div>
+            { voirDemandes && (   
+                <div className="overlay ">
+                    <VoirDemandes 
+                    selectedList={selectedList}
+                    pseudoUser={pseudoUser}
+                    handleClose={handleClose}
+                    />
                 </div>
-                <div className='flex flex-wrap align-left justify-left p-3 m-3'>
-                    <ul>
-                        {selectedList.map((item, index) => (
-                            <li className={` ${item.archive === 1 ? "bg-indigo-100" : "bg-fuchsia-100"} flex flex-row justify-between p-3 m-4 rounded-xl`} key={index}>
-                                <div className='ml-2 flex flex-col'>
-                                    <span>
-                                        Créneau : 
-                                    </span>
-                                    <span>
-                                        {creneauxInfo[item.creneau_id]}
-                                    </span>
-                                </div>
-                                <div className='mx-2 ml-14 flex flex-col    '>
-                                    <span>
-                                        Zone :
-                                    </span>
-                                    <span>
-                                        {zoneInfo[item.zonebenevole_id]}
-                                    </span>
-                                </div>
-                                {
-                                    item.accepte === 0 && item.archive === 0 ? (
-                                        <button className="ml-10 rounded-full bg-lime-600 px-4 py-2 text-white hover:bg-indigo-700 transition-all" onClick={() => handleSubmitValide(item.id)}>
-                                            Valider
-                                        </button>
-                                    ) :
-                                        (
-                                            <button className="ml-3 rounded-full bg-indigo-600 px-4 py-2 text-white hover:bg-fuchsia-500 transition-all" onClick={() => handleSubmitDevalide()}>
-                                                Supprimer
-                                            </button>
-                                        )
-
-                                }
-
-                            </li>
-                        ))}
-                    </ul>
+            )}
+            <div className={` ${voirDemandes && 'blur'} flex flex-wrap align-center justify-center`} >
+                <h1>Liste des demandes d'activités</h1>
+                <div className='flex flex-wrap align-center justify-center'>
+                    {Object.entries(usersWithDemands).map(([userId, demands]) => (
+                        <div className='m-4 flex flex-wrap align-center justify-center cursor-pointer overflow-hidden relative transition-all duration-500 hover:translate-y-2 w-72 h-50 bg-neutral-50 rounded-lg shadow-xl  items-center justify-evenly gap-2 p-2 before:absolute before:w-full hover:before:top-0 before:duration-500 before:-top-1 before:h-1 before:bg-indigo-500' key={userId}>
+                            <figure className="mx-auto my-8 flex h-32 w-32 items-center justify-center rounded-full bg-indigo-800 ">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="currentColor" className="bi bi-person-fill text-white " viewBox="0 0 16 16">
+                                    <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"></path>
+                                </svg>
+                            </figure>
+                            <h1 className='text-xl mx-4'>Demande de: {users[userId].pseudo}</h1>
+                            <div className='flex flex-wrap align-center justify-center'>
+                                <button className="text-sm rounded-full bg-lime-600 px-3 py-2.5 text-white hover:bg-lime-500 mt-3 mr-2 my-3" onClick={() => handleClickDemandes(demands,users[userId].pseudo)}>
+                                        Voir demandes
+                                </button>
+                                <button className="text-sm rounded-full bg-fuchsia-600 px-3 py-2.5 text-white hover:bg-fuchsia-500 mt-3 my-3" >
+                                        Profil
+                                </button>
+                            </div>
+                        </div>
+                    ))}
                 </div>
-            </div>
-            <div className="flex items-center justify-between pl-[380px]">
-
             </div>
         </div>
     );
 };
 
-export default VoirDemandes;
+export default GestionDemandes;
